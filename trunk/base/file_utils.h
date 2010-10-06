4bfe341c49cab3de45fe9f023fad0cb046761c78
legabox <legabox@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-10-06 14:17:53 +0000

    file utils functions

---
+++ trunk/base/file_utils.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2010 Timur Iskhodzhanov and others. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+#ifndef BASE_FILE_UTILS_H_
+#define BASE_FILE_UTILS_H_
+#pragma once
+
+#include <stdio.h>
+
+#include <string>
+
+// Read text file into string *contents.
+// Returns true on success, false on failure.
+bool ReadFileToString(const std::string &filename, std::string *contents);
+
+// Write string to text file.
+// Returns true on success, false on failure.
+bool WriteStringAs(const std::string &filename, const std::string &contents);
+
+// Returns true if path exists.
+bool PathExists(const std::string &path);
+
+// Returns true if all the operations of the mode are permitted.
+bool PathAccessPermitted(const std::string &path, int mode);
+
+#endif  // BASE_FILE_UTILS_H_
