a58b38aec596131c18195a62d3ad026812d4edfd
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-10-06 09:22:19 +0000

    Extend the copyright on all files

--- trunk/base/random.h
+++ trunk/base/random.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 Timur Iskhodzhanov. All rights reserved.
+// Copyright (c) 2010 Timur Iskhodzhanov and others. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #ifndef BASE_RANDOM_H_
