3fed58e46ca4c460c418c43fb61f15fd45b69127
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-10-28 19:51:49 +0000

    Add CHECK/ASSERT/EXPECT demo to base/check_test.cpp

--- base/check_test.cpp
+++ base/check_test.cpp
@@ -10,6 +10,17 @@ TEST(CheckTest, CheckTrueSucceedsTest) {
   CHECK(42);
 }
 
+TEST(CheckTest, AssertionsAndChecksTest) {
+  CHECK(2 + 2 == 4);
+  ASSERT_TRUE(2 + 2 == 4);
+  ASSERT_EQ(2 + 2, 4);
+  ASSERT_LE(2 + 2, 5);
+  printf("Passed all ASSERT macros, now EXPECT macros\n");
+  EXPECT_TRUE(2 + 2 == 4);
+  EXPECT_LE(2 + 2, 5);
+  printf("End of test\n");
+}
+
 // C preprocessor magic, see
 // http://www.decompile.com/cpp/faq/file_and_line_error_string.htm
 #define STRINGIFY(x) #x
