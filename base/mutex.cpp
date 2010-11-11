f018e7afddd3d47dbb930a7f2cfb6ead94a20f46
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-11-11 11:05:31 +0000

    Replace CHECK(a == b) with CHECK_EQ(a, b)

--- base/mutex.cpp
+++ base/mutex.cpp
@@ -9,34 +9,34 @@ namespace threading {
 Mutex::Mutex() {
 #ifndef NDEBUG
   pthread_mutexattr_t attr;
-  CHECK(pthread_mutexattr_init(&attr) == 0);
+  CHECK_EQ(pthread_mutexattr_init(&attr), 0);
 
   // If a thread attempts to relock a mutex that it has already locked,
   // an error shall be returned. If a thread attempts to unlock a mutex
   // that it has not locked or a mutex which is unlocked, an error
   // shall be returned.
-  CHECK(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK) == 0);
-  CHECK(pthread_mutex_init(&mutex_, &attr) == 0);
-  CHECK(pthread_mutexattr_destroy(&attr) == 0);
+  CHECK_EQ(pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK), 0);
+  CHECK_EQ(pthread_mutex_init(&mutex_, &attr), 0);
+  CHECK_EQ(pthread_mutexattr_destroy(&attr), 0);
   is_locked_ = false;
 #else
-  CHECK(pthread_mutex_init(&mutex_, NULL) == 0);
+  CHECK_EQ(pthread_mutex_init(&mutex_, NULL), 0);
 #endif
 }
 
 Mutex::~Mutex() {
 #ifndef NDEBUG
-  CHECK(is_locked_ == false);
+  CHECK_EQ(is_locked_, false);
 #endif
-  CHECK(pthread_mutex_destroy(&mutex_) == 0);
+  CHECK_EQ(pthread_mutex_destroy(&mutex_), 0);
 }
 
 void Mutex::Lock() {
   // Since we have PTHREAD_MUTEX_ERRORCHECK mutex type, we need not perform
   // twice-lock checks in debug mode.
-  CHECK(pthread_mutex_lock(&mutex_) == 0);
+  CHECK_EQ(pthread_mutex_lock(&mutex_), 0);
 #ifndef NDEBUG
-  CHECK(is_locked_ == false);
+  CHECK_EQ(is_locked_, false);
   is_locked_ = true;
 #endif
 }
@@ -45,7 +45,7 @@ bool Mutex::TryLock() {
   bool result = (pthread_mutex_trylock(&mutex_) == 0);
 #ifndef NDEBUG
   if (result) {
-    CHECK(is_locked_ == false);
+    CHECK_EQ(is_locked_, false);
     is_locked_ = true;
   }
 #endif
@@ -54,10 +54,10 @@ bool Mutex::TryLock() {
 
 void Mutex::Unlock() {
 #ifndef NDEBUG
-  CHECK(is_locked_ == true);
+  CHECK_EQ(is_locked_, true);
   is_locked_ = false;
 #endif
-  CHECK(pthread_mutex_unlock(&mutex_) == 0);
+  CHECK_EQ(pthread_mutex_unlock(&mutex_), 0);
 }
 
 }  // namespace threading
