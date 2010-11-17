8403ec2e666a00f88f9c33c096706194e75f65af
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-11-17 17:47:47 +0000

    ... one more

--- trunk/target_template.mk
+++ trunk/target_template.mk
@@ -5,7 +5,7 @@
 # TODO(timurrrr): it's probably better to list files manually in
 # target-specific .mk's. We should do that when our codebase becomes
 # more-or-less stable.
-# This will be changed when we migrate to CMake, see
+# This will be changed when we migrate to CMake, please see
 # http://code.google.com/p/mipt-course/issues/detail?id=1
 
 # Get the list of ".h" files for the given TARGET
