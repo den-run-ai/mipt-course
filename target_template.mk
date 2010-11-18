b058325843ae3368152f4564dcd0e8fe7f4dc6a1
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-11-18 22:10:11 +0000

    Add sandbox/ directory by introducing some extra ugliness to our makefiles

--- target_template.mk
+++ target_template.mk
@@ -5,39 +5,42 @@
 # TODO(timurrrr): it's probably better to list files manually in
 # target-specific .mk's. We should do that when our codebase becomes
 # more-or-less stable.
+# TODO(timurrrr) Also, the $(TARGET)_ prefix is ugly.
+# Makefile gurus are welcome to fix this more elegantly.
+#
 # This will be changed when we migrate to CMake, see
 # (see http://code.google.com/p/mipt-course/issues/detail?id=1 )
 
 # Get the list of ".h" files for the given TARGET
-H_LIST=$(wildcard $(TARGET)/*.h)
+$(TARGET)_H_LIST=$(wildcard $(TARGET)/*.h)
 
 # Get the list of ".cpp" files and trim the ".cpp" extension
 # TEST_CPP_LIST will contain *_test.cpp files
 # LIB_CPP_LIST  will contain all other .cpp files
-ALL_CPP_LIST=$(patsubst %.cpp,%,$(wildcard $(TARGET)/*.cpp))
-TEST_CPP_LIST=$(patsubst %.cpp,%,$(wildcard $(TARGET)/*_test.cpp))
-LIB_CPP_LIST=$(filter-out $(TEST_CPP_LIST), $(ALL_CPP_LIST))
+$(TARGET)_ALL_CPP_LIST=$(patsubst %.cpp,%,$(wildcard $(TARGET)/*.cpp))
+$(TARGET)_TEST_CPP_LIST=$(patsubst %.cpp,%,$(wildcard $(TARGET)/*_test.cpp))
+$(TARGET)_LIB_CPP_LIST=$(filter-out $($(TARGET)_TEST_CPP_LIST), $($(TARGET)_ALL_CPP_LIST))
 
 # Turn all "TARGET/X.cpp" into "out/TARGET_X.obj"
-LIB_OBJ_LIST=$(patsubst %,$(OUTDIR)%.$(OBJ),$(LIB_CPP_LIST))
-TEST_OBJ_LIST=$(patsubst %,$(OUTDIR)%.$(OBJ),$(TEST_CPP_LIST))
+$(TARGET)_LIB_OBJ_LIST=$(patsubst %,$(OUTDIR)%.$(OBJ),$($(TARGET)_LIB_CPP_LIST))
+$(TARGET)_TEST_OBJ_LIST=$(patsubst %,$(OUTDIR)%.$(OBJ),$($(TARGET)_TEST_CPP_LIST))
 
-TARGET_DIR=$(OUTDIR)$(TARGET)
-$(TARGET_DIR):
-	mkdir -p $(TARGET_DIR)
+$(TARGET)_DIR=$(OUTDIR)$(TARGET)
+$($(TARGET)_DIR):
+	mkdir -p $@
 
 # "$<" stands for "first dependency"
 # "$@" stands for "rule name"
 # TODO(timurrrr): smarter .h dependencies, e.g. "g++ -MM"
-$(OUTDIR)$(TARGET)/%.$(OBJ): $(TARGET)/%.cpp $(H_LIST) | $(TARGET_DIR)
+$(OUTDIR)$(TARGET)/%.$(OBJ): $(TARGET)/%.cpp $($(TARGET)_H_LIST) | $($(TARGET)_DIR)
 	$(CC) $(CC_FLAGS) -o $@ -c $<
 
 # "$@" stands for "rule name"
 # "$^" stands for "all dependencies
 # TODO(timurrrr): this fails when there aren't any OBJs for this target
-$(OUTDIR)lib$(TARGET).a: $(LIB_OBJ_LIST)
+$(OUTDIR)lib$(TARGET).a: $($(TARGET)_LIB_OBJ_LIST)
 	$(AR) $@ $^
 
-$(OUTDIR)$(TARGET)_tests: $(TEST_OBJ_LIST) $(OUTDIR)lib$(TARGET).a \
+$(OUTDIR)$(TARGET)_tests: $($(TARGET)_TEST_OBJ_LIST) $(OUTDIR)lib$(TARGET).a \
 			  $(OUTDIR)libgoogletest.a
 	$(LINK) $(LINK_FLAGS) -o $@ $^
