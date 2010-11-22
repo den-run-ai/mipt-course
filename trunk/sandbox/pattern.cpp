264dd51fc0caaf0391fd6d0dfc8f7e5a3554f552
bochkarev.alex@gmail.com <bochkarev.alex@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-11-22 15:26:37 +0000

    Added StringPattern stub and tests for it. Reviewed at http://codereview.appspot.com/3224041

---
+++ trunk/sandbox/pattern.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2010 Timur Iskhodzhanov and MIPT students. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sandbox/pattern.h"
+
+#include <string>
+
+bool StringPattern::Match(const std::string &str) const {
+  // TODO(bochkarev.alex): implement.
+  return false;
+}
+
