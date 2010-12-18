fdfea01ca47555780642d9ca5e73cfb1b419290c
bochkarev.alex@gmail.com <bochkarev.alex@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-12-18 13:07:28 +0000

    Fix/suppress cpplint warnings. Reviewed at http://codereview.appspot.com/3639042/

--- base/common.h
+++ base/common.h
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // Use CHECK instead of assert for the following reasons:
 // a) CHECKs remain when NDEBUG is defined (use DCHECK for debug-only checks)
@@ -31,8 +32,16 @@
 
 // TODO(timurrrr): define proper CHECK_EQ, DCHECK_EQ, ... printing their args
 // in case of failure.
-#define CHECK_EQ(x,y) CHECK((x) == (y))
-#define DCHECK_EQ(x,y) DCHECK((x) == (y))
+#define CHECK_EQ(x,y) CHECK((x) == (y))     // NOLINT
+#define CHECK_GT(x,y) CHECK((x) > (y))      // NOLINT
+#define CHECK_GE(x,y) CHECK((x) >= (y))     // NOLINT
+#define CHECK_LT(x,y) CHECK((x) < (y))      // NOLINT
+#define CHECK_LE(x,y) CHECK((x) <= (y))     // NOLINT
+#define DCHECK_EQ(x,y) DCHECK((x) == (y))   // NOLINT
+#define DCHECK_GT(x,y) DCHECK((x) > (y))    // NOLINT
+#define DCHECK_GE(x,y) DCHECK((x) >= (y))   // NOLINT
+#define DCHECK_LT(x,y) DCHECK((x) < (y))    // NOLINT
+#define DCHECK_LE(x,y) DCHECK((x) <= (y))   // NOLINT
 
 // Use this macro to disable copy constructor and operator= for CLASS_NAME. See
 // http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Copy_Constructors#Copy_Constructors
@@ -42,11 +51,11 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-typedef unsigned char  uchar;
-typedef unsigned char  byte;
-typedef unsigned short ushort;
-typedef unsigned int   uint;
-const uint MAX_UINT = 0xFFFFFFFF;
+typedef unsigned char uchar;
+typedef unsigned char byte;
+typedef uint16_t      uint16;
+typedef uint32_t      uint32;
+const uint32 MAX_UINT32 = 0xFFFFFFFF;
 // TODO(timurrrr): add 64-bit and ptr type values when necessary.
 
 #endif  // BASE_COMMON_H_
