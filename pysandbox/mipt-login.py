185fc750665f9cb36ec274d98ede41ace643ce71
kazeevn@gmail.com <kazeevn@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-12-18 15:16:37 +0000

    Added reviewers list. Changed header to /usr/bin/env.

--- pysandbox/mipt-login.py
+++ pysandbox/mipt-login.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2010, Nikita Kazeev
+# Copyright 2010, Nikita Kazeev with special thanks to
+# Timur Iskhodzhanov
+# Alexander Travov
+# Alex Bochkarev
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
