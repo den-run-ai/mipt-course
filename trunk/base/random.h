25104074941db6e69fce2f1f2beb4f1d0e84f094
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2010-12-21 15:30:33 +0000

    Rewrite base/random.h - remove redundant functions, add a loooong comment; Reviewed at http://codereview.appspot.com/3754044

--- trunk/base/random.h
+++ trunk/base/random.h
@@ -7,41 +7,48 @@
 
 #include "base/common.h"
 
-// Seed is required to make TrueRandom thread safe.
+// rand() is not thread-safe, hence it is FORBIDDEN in our codebase.
+// rand_r(&seed) is thread-safe if seed is thread-local.
 // Visit http://stackoverflow.com/questions/3973665 for more information.
-// Generally consider using RandomGenerator instead of TrueRandom.
-template<typename T>
-inline T TrueRandom(uint32 *seed) {
-  // TODO(timurrrr): better ideas?
-  const size_t RAND_BITS = 15;
-  T ret = 0;
-  for (size_t i = 0; i < sizeof(T); i += RAND_BITS) {
-    ret = (ret << RAND_BITS) | (rand_r(seed) & ((1 << RAND_BITS) - 1));
-  }
-  return ret;
-}
-
-inline uint32 RandomUInt32(uint32 *seed) { return TrueRandom<uint32>(seed); }
-inline uint16 RandomUInt16(uint32 *seed) { return TrueRandom<uint16>(seed); }
-inline byte   RandomByte(uint32 *seed)   { return TrueRandom<byte>(seed);   }
-
+//
+// To achieve "true" randomness, you should use a random seed
+// (e.g initial seed=current time).
+// However, this is not good for continuous testing environment since this
+// may cause your tests to produce diffent results
+// (e.g. fail once in every 100 runs, very likely on unrelated commits).
+//
+// RandomGenerator is a class to add determinism to "random" code when it is
+// run in test mode.
+// If you want to write a test with semi-random numbers consider using a scoped
+// instance of this class with a hard-coded initial seed to achieve
+// repeatability.
+// If you want to write a class that has some semi-random algorithm please
+// consider specifying a RandomGenerator as a constructor argument and
+// DON'T use rand_r() directly.
+//
 // This class allows thread safe random generation in the sense that
 // two threads can safely call Generate() method of two __different__
 // RandomGenerator objects simultaneously.
 // This class is NOT THREAD-SAFE since calling Generate() method of one
 // class instance from two threads will result in a data race.
 // Hence don't use one instance of this class in more than one thread!
-// It is recommended to use a scoped RandomGenerator if you want to write a test
-// with semi-random numbers to achieve repeatability.
 class RandomGenerator {
  public:
   explicit RandomGenerator(uint32 seed = 1) : seed_(seed) {}
   ~RandomGenerator() {}
 
   // Returns a random object of type T.
+  // TODO(timurrrr): it's very likely this template doesn't work well with
+  // floats and doubles and more complex types.
   template<typename T>
   T Generate() {
-    return TrueRandom<T>(&seed_);
+    // TODO(timurrrr): better ideas?
+    const size_t RAND_BITS = 15;
+    T ret = 0;
+    for (size_t i = 0; i < sizeof(T); i += RAND_BITS) {
+      ret = (ret << RAND_BITS) | (rand_r(&seed_) & ((1 << RAND_BITS) - 1));
+    }
+    return ret;
   }
 
   // Returns the seed that will be used in the next call to Generate().
