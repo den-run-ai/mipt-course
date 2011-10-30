c0b8c8a72e58a6b185cd52fc95b405bd8838c3fc
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-10-30 17:43:37 +0000

    Bulk update copyright + AUTHORS file

--- trunk/testing/googletest_main.cpp
+++ trunk/testing/googletest_main.cpp
@@ -1,8 +1,8 @@
-// Copyright (c) 2010 Timur Iskhodzhanov and MIPT students. All rights reserved.
+// Copyright (c) 2011 Timur Iskhodzhanov and MIPT students. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/googletest/include/gtest/gtest.h"
+#include "gtest/gtest.h"
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
