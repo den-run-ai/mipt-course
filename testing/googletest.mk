24f0e045226b41f75a9b079cc9a6686d80bd66c0
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-10-30 17:34:37 +0000

    Okay, un-break the Makefile build

--- testing/googletest.mk
+++ testing/googletest.mk
@@ -2,15 +2,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-GTEST_ROOT=third_party/googletest
+GMOCK_ROOT=third_party/googlemock
+GTEST_ROOT=third_party/googlemock/gtest
 # Add googletest to the include path.
-CC_FLAGS+=-I$(GTEST_ROOT)/include
+CC_FLAGS+=-I$(GMOCK_ROOT)/include -I$(GTEST_ROOT)/include
 
+GMOCK_LIB=$(OUTDIR)libgooglemock.a
 GTEST_LIB=$(OUTDIR)libgoogletest.a
 
 $(OUTDIR)testing/googletest_main.obj: testing/googletest_main.cpp
 	mkdir -p $(OUTDIR)testing
 	$(CC) $(CC_FLAGS) -o $@ -c $<
+
+$(GMOCK_LIB): $(OUTDIR)testing/googletest_main.obj
+	make -C $(GMOCK_ROOT)/make gmock-all.o
+	$(AR) $@ $(GMOCK_ROOT)/make/gmock-all.o $^
 
 $(GTEST_LIB): $(OUTDIR)testing/googletest_main.obj
 	make -C $(GTEST_ROOT)/make gtest-all.o
