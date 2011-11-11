6b44ae8f7b904f816825f2e4ef497bde0dd58570
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-11-11 19:04:15 +0000

    Rename the python_demo_test + add some comments

--- pysandbox/python_unittest_demo.py
+++ pysandbox/python_unittest_demo.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# http://docs.python.org/library/unittest.html
+
 import random
 import unittest
 
@@ -13,10 +15,20 @@ class SimplePythonTest(unittest.TestCase):
 # Define a fixture:
 class SimpleFixtureTest(unittest.TestCase):
   def setUp(self):
+    # Runs before each test 
     self.seq = range(10)
 
   def tearDown(self):
+    # Runs after each test
     pass
+
+  @classmethod  # Python 2.7+
+  def setUpClass(cls):
+    print "Runs once before starting the first test from this fixture"
+
+  @classmethod  # Python 2.7+
+  def tearDownClass(cls):
+    print "Runs once after starting the first test from this fixture"
 
   # ... and some tests using this fixture:
   def test_shuffle(self):
