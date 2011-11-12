4359cca1d035a5956140d8f0020d5fa18c705bca
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-11-12 18:21:23 +0000

    Add __main__ demo

---
+++ demos/python/B_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# run it from command line:
+#   $ python B_main.py
+# and then from interactive python shell:
+#   $ python
+#   >>> import B_main
+# and compare the results.
+
+print "Hello, this is B_main.py"
+
+if __name__ == "__main__":
+  print "You ran `python B_main.py`!"
+else:
+  print "You've imported me!"
