b431d3873d05f05a4cb81330670c1ea982db8145
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-11-21 10:45:21 +0000

    Add a test that prints out the current build type

---
+++ trunk/base/common_test.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2011 Timur Iskhodzhanov and MIPT students. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gtest/gtest.h"
+
+#include "base/common.h"
+
+TEST(CommonTest, DebugOrRelease) {
+#ifdef NDEBUG
+  printf("NDEBUG defined, this is a Release build\n");
+#else
+  printf("NDEBUG is not defined, this is a Debug build\n");
+#endif
+}
