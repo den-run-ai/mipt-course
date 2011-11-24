0649765634665d2192a54be0adcdbe9e1062db71
pogr.yuo@gmail.com <pogr.yuo@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-11-24 14:11:00 +0000

    Red-black tree put and get tests added. Reviewed at http://codereview.appspot.com/5415056/

--- sandbox/red_black_tree.h
+++ sandbox/red_black_tree.h
@@ -10,16 +10,22 @@
 template<typename KeyType, typename ValueType>
 class RBTree {
  public:
-  RBTree();
+  RBTree() {
+    NOT_IMPLEMENTED;
+  }
 
-  ~RBTree();
+  ~RBTree() {
+    NOT_IMPLEMENTED;
+  }
 
   // Add node with specified key and value.
-  void put(const KeyType &key, const ValueType &value);
+  void put(const KeyType &key, const ValueType &value) {
+    NOT_IMPLEMENTED;
+  }
 
   /* Remove node with specified key. 
      Return false if the key did not exist. */
-  bool remove(const KeyType &key) {
+  bool remove(KeyType key) {
     NOT_IMPLEMENTED;
     return false;
   }
@@ -27,14 +33,16 @@ class RBTree {
   /* Remove all nodes, which contain specified value.
      Return false if the value did not exist. 
      Algorhithmic complexity is O(n), where n - number of nodes in the tree. */
-  bool removeValues(const ValueType &value) {
+  bool removeValues(ValueType value) {
     NOT_IMPLEMENTED;
     return false;
   }
 
   // TODO(pogr.yuo): Const-version.
-  // Return false if key does not exist, otherwise return true
-  bool get(const KeyType &key, ValueType *value_out);
+  ValueType get(KeyType key) {
+    NOT_IMPLEMENTED;
+    return ValueType();
+  }
 
  private:
   struct Node {
@@ -44,38 +52,9 @@ class RBTree {
     Node* parent;
     Node* left;
     Node* right;
-
-    Node() {
-      color = kRed;
-      parent = NULL;
-      left = NULL;
-      right = NULL;
-    }
   };
 
-  enum Colors {
-    kBlack = 0,
-    kRed = 1,
-  };
-
-  // delete all children of specified node
-  void deleteChildren(Node* start_node);
-
-  Node* find(const KeyType &key) {
-    Node* node = root_;
-    while (node != NULL && key != node->key) {
-      if (key < node->key) {
-        node = node->left;
-      } else {
-        node = node->right;
-      }
-    }
-    return node;
-  };
-
-  Node *root_;
-
-  DISALLOW_COPY_AND_ASSIGN(RBTree);
+  Node* root;
 };
 
 #endif  // SANDBOX_RED_BLACK_TREE_H_
--- sandbox/red_black_tree_test.cpp
+++ sandbox/red_black_tree_test.cpp
@@ -3,55 +3,33 @@
 // found in the LICENSE file.
 
 #include "gtest/gtest.h"
-#include "base/random.h"
 #include "sandbox/red_black_tree.h"
-#include "sandbox/red_black_tree-inl.h"
 
-TEST(RBTree, PutGetTest) {
+TEST(RBTreeTest, DISABLED_PutTest) {
   RBTree<int, int> tree;
-  int value = NULL;
-
-  EXPECT_FALSE(tree.get(0, &value));
 
   tree.put(-1, -100);
   tree.put(0, 0);
   tree.put(1, 100);
 
-  EXPECT_TRUE(tree.get(-1, &value));
-  EXPECT_EQ(-100, value);
-  EXPECT_TRUE(tree.get(0, &value));
-  EXPECT_EQ(0, value);
-  EXPECT_TRUE(tree.get(1, &value));
-  EXPECT_EQ(100, value);
-
-  tree.put(0, 1);
-  EXPECT_TRUE(tree.get(0, &value));
-  EXPECT_EQ(1, value);
+  EXPECT_EQ(-100, tree.get(-1));
+  EXPECT_EQ(0, tree.get(0));
+  EXPECT_EQ(100, tree.get(1));
 }
 
-TEST(RBTree, PutStressTest) {
+TEST(RBTreeTest, DISABLED_RemoveTest) {
   RBTree<int, int> tree;
-  const int kNumberOfNodes = 10000;
-  RandomGenerator rg;
-  for (int i = 0; i < kNumberOfNodes; i++) {
-    tree.put(i, rg.Generate<int>());
-  }
-}
-
-TEST(RBTree, DISABLED_RemoveTest) {
-  RBTree<int, int> tree;
-  int value = NULL;
 
   tree.put(0, 0);
   tree.put(1, 0);
   tree.put(2, 1);
 
   ASSERT_TRUE(tree.remove(2));
-  ASSERT_FALSE(tree.get(2, &value));
+  ASSERT_FALSE(tree.get(2));
 
   ASSERT_TRUE(tree.removeValues(0));
-  ASSERT_FALSE(tree.get(0, &value));
-  ASSERT_FALSE(tree.get(1, &value));
+  ASSERT_FALSE(tree.get(0));
+  ASSERT_FALSE(tree.get(1));
 
   ASSERT_FALSE(tree.remove(0));
   ASSERT_FALSE(tree.removeValues(0));
