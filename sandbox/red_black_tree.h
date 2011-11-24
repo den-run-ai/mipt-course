fa85dfe409b2ab4be56960087b29487f1579598d
pogr.yuo@gmail.com <pogr.yuo@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-11-24 14:13:31 +0000

    Red-black tree put and get tests added. Reviewed at http://codereview.appspot.com/5415056/

--- sandbox/red_black_tree.h
+++ sandbox/red_black_tree.h
@@ -10,22 +10,16 @@
 template<typename KeyType, typename ValueType>
 class RBTree {
  public:
-  RBTree() {
-    NOT_IMPLEMENTED;
-  }
+  RBTree();
 
-  ~RBTree() {
-    NOT_IMPLEMENTED;
-  }
+  ~RBTree();
 
   // Add node with specified key and value.
-  void put(const KeyType &key, const ValueType &value) {
-    NOT_IMPLEMENTED;
-  }
+  void put(const KeyType &key, const ValueType &value);
 
   /* Remove node with specified key. 
      Return false if the key did not exist. */
-  bool remove(KeyType key) {
+  bool remove(const KeyType &key) {
     NOT_IMPLEMENTED;
     return false;
   }
@@ -33,16 +27,14 @@ class RBTree {
   /* Remove all nodes, which contain specified value.
      Return false if the value did not exist. 
      Algorhithmic complexity is O(n), where n - number of nodes in the tree. */
-  bool removeValues(ValueType value) {
+  bool removeValues(const ValueType &value) {
     NOT_IMPLEMENTED;
     return false;
   }
 
   // TODO(pogr.yuo): Const-version.
-  ValueType get(KeyType key) {
-    NOT_IMPLEMENTED;
-    return ValueType();
-  }
+  // Return false if key does not exist, otherwise return true
+  bool get(const KeyType &key, ValueType *value_out);
 
  private:
   struct Node {
@@ -52,9 +44,38 @@ class RBTree {
     Node* parent;
     Node* left;
     Node* right;
+
+    Node() {
+      color = kRed;
+      parent = NULL;
+      left = NULL;
+      right = NULL;
+    }
   };
 
-  Node* root;
+  enum Colors {
+    kBlack = 0,
+    kRed = 1,
+  };
+
+  // delete all children of specified node
+  void deleteChildren(Node* start_node);
+
+  Node* find(const KeyType &key) {
+    Node* node = root_;
+    while (node != NULL && key != node->key) {
+      if (key < node->key) {
+        node = node->left;
+      } else {
+        node = node->right;
+      }
+    }
+    return node;
+  };
+
+  Node *root_;
+
+  DISALLOW_COPY_AND_ASSIGN(RBTree);
 };
 
 #endif  // SANDBOX_RED_BLACK_TREE_H_
