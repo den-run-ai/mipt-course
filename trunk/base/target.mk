4b0ec8b538c6005159a520013749508717869113
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-12-02 16:26:27 +0000

    Goodbye, Makefiles\!

--- trunk/base/target.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2011 Timur Iskhodzhanov and MIPT students. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-TARGET=base
-
-ADDITIONAL_LIBS+=pthread
-
-include target_template.mk
