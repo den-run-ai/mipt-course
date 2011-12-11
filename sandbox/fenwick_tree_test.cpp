a7f5479c00d0cb8e5ca1b543995f3770e4035514
alex.y.taran <alex.y.taran@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-12-11 21:08:00 +0000

    Added a random-generation test to fenwick tree tests, also added some functions in class RandomGenerator for generating numbers in specific range

--- sandbox/fenwick_tree_test.cpp
+++ sandbox/fenwick_tree_test.cpp
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
 #include "gtest/gtest.h"
 
 #include "base/common.h"
+#include "base/random.h"
 
 #include "sandbox/fenwick_tree.h"
 
@@ -25,7 +27,7 @@ TEST(FenwickTreeTest, InsertRemoveElementsTest) {
   EXPECT_EQ(0u, fnw.size());
 }
 
-TEST(FenwickTreeTest, CountElementsTest) {
+TEST(FenwickTreeTest, SimpleCountElementsTest) {
   FenwickTree<int> fnw;
   for (int i = 0; i < 10; ++i)
     fnw.push_back(i);
@@ -41,6 +43,33 @@ TEST(FenwickTreeTest, CountElementsTest) {
 
   for (int i = 0; i < 8; ++i)
     EXPECT_EQ(i, fnw.get(i));
+}
+
+TEST(FenwickTreeTest, RandomizedCountElementsTest) {
+  RandomGenerator gen(42 * 42);
+  FenwickTree<int> fnw;
+  std::vector<int> arr;
+
+  const int ARRAY_SIZE = 257;
+
+  for (int i = 0; i < ARRAY_SIZE; ++i) {
+    int k = gen.Generate<int>();
+    fnw.push_back(k);
+    arr.push_back(k);
+  }
+
+  for (int i = 0; i < 20000; ++i) {
+    int left = gen.Generate<int>(ARRAY_SIZE);
+    int right = gen.Generate<int>(left, ARRAY_SIZE);
+
+    int fnw_sum = fnw.count(left, right);
+
+    int arr_sum = 0;
+    for (int j = left; j < right; ++j)
+      arr_sum += arr[j];
+
+    ASSERT_EQ(arr_sum, fnw_sum);
+  }
 }
 
 TEST(FenwickTreeTest, DISABLED_ModifyElements) {
