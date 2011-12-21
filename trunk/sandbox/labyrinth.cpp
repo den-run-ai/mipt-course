18a02d493d38a10a72328332481f404042aee019
acbelter@gmail.com <acbelter@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-12-21 22:34:47 +0000

    Implement Lee's algorithm for the Labyrinth class. Reviewed at http://codereview.appspot.com/5488096

--- trunk/sandbox/labyrinth.cpp
+++ trunk/sandbox/labyrinth.cpp
@@ -4,14 +4,99 @@
 
 #include "sandbox/labyrinth.h"
 
-// TODO(acbelter): Write the constructor.
-Labyrinth::Labyrinth(int *field, int height, int width) { }
+#include <algorithm>
+#include <vector>
+#include <map>
 
-Labyrinth::~Labyrinth() { }
+void Labyrinth::GetAdjacentCells(const Cell &cell,
+                                 std::vector<Cell> *adjacent_cells) const {
+  Cell up_cell = Cell(cell.x, cell.y - 1);
+  Cell right_cell = Cell(cell.x + 1, cell.y);
+  Cell down_cell = Cell(cell.x, cell.y + 1);
+  Cell left_cell = Cell(cell.x - 1, cell.y);
 
-bool Labyrinth::FindPath(const LabyrinthPoint &start_point,
-                         const LabyrinthPoint &end_point,
+  adjacent_cells->clear();
+
+  if (cell.y != 0 && IsEmpty(up_cell)) {
+    adjacent_cells->push_back(up_cell);
+  }
+  if (cell.x != this->width() - 1 && IsEmpty(right_cell)) {
+    adjacent_cells->push_back(right_cell);
+  }
+  if (cell.y != this->height() - 1 && IsEmpty(down_cell)) {
+    adjacent_cells->push_back(down_cell);
+  }
+  if (cell.x != 0 && IsEmpty(left_cell)) {
+    adjacent_cells->push_back(left_cell);
+  }
+}
+
+bool Labyrinth::FindPath(const Cell &start_cell,
+                         const Cell &end_cell,
                          Path *found_path) const {
-  // TODO(acbelter): Write this function.
-  return false;
+  // For path search the wave algorithm (Lee's algorithm) is used.
+  // See http://en.wikipedia.org/wiki/Lee_algorithm
+
+  if (!IsEmpty(start_cell) || !IsEmpty(end_cell)) {
+    return false;
+  }
+
+  if (start_cell == end_cell) {
+    found_path->append(start_cell);
+    return true;
+  }
+
+  std::map<Cell, int> visited_cells;
+  std::vector<Cell> current_front;
+  int front_value = 1;
+
+  visited_cells[start_cell] = front_value;
+  GetAdjacentCells(start_cell, &current_front);
+
+  bool reached_end = false;
+
+  std::vector<Cell> next_front, temp;
+  while (!reached_end) {
+    next_front.clear();
+    std::vector<Cell>::iterator it;
+    for (it = current_front.begin(); it != current_front.end(); ++it) {
+      if (visited_cells[*it] != 0) {
+        continue;
+      }
+      visited_cells[*it] = front_value + 1;
+      GetAdjacentCells(*it, &temp);
+      next_front.insert(next_front.end(), temp.begin(), temp.end());
+    }
+
+    front_value++;
+
+    if (visited_cells[end_cell] != 0) {
+      reached_end = true;
+    }
+
+    if (current_front.empty()) {
+      return false;
+    }
+
+    swap(current_front, next_front);
+  }
+
+  // Backtraces a path from the end_cell to the start_cell.
+  Cell current_cell = end_cell;
+  found_path->append(current_cell);
+  while (front_value != 1) {
+    GetAdjacentCells(current_cell, &current_front);
+    std::vector<Cell>::iterator it;
+    for (it = current_front.begin(); it != current_front.end(); ++it) {
+      if (visited_cells[*it] == front_value - 1) {
+        found_path->append(*it);
+        current_cell = *it;
+        front_value--;
+        break;
+      }
+    }
+    CHECK(it != current_front.end());
+  }
+  found_path->reverse();
+  return true;
 }
