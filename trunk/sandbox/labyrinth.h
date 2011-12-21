000287d2c04837b0e5e866d72a09e425cb1b38cb
acbelter@gmail.com <acbelter@gmail.com@25c5acd0-bda5-8b57-2d56-82901bac209b>
2011-12-21 21:37:43 +0000

    Revert r324 - failed on the buildbot

--- trunk/sandbox/labyrinth.h
+++ trunk/sandbox/labyrinth.h
@@ -5,49 +5,23 @@
 #ifndef SANDBOX_LABYRINTH_H_
 #define SANDBOX_LABYRINTH_H_
 
-#include <string.h>
-
 #include <utility>
-#include <vector>
 #include <list>
-#include <map>
 
 #include "base/common.h"
 
-struct Cell {
-  Cell(int x_, int y_): x(x_), y(y_) { }
+typedef std::pair<int, int> LabyrinthPoint;
 
-  bool operator== (const Cell &cell) const {
-    return (this->x == cell.x && this->y == cell.y);
-  }
-
-  bool operator< (const Cell &cell) const {
-    if (this->x < cell.x) {
-      return true;
-    } else if (this->x > cell.x) {
-      return false;
-    } else {
-      return this->y < cell.y;
-    }
-  }
-
-  int x, y;
-};
-
-class Path : public std::list<Cell> {
+class Path : std::list<LabyrinthPoint> {
  public:
   Path() { }
 
-  bool operator== (const Path &path) const {
+  void append(int first_index, int second_index) {
+    this->push_back(LabyrinthPoint(first_index, second_index));
+  }
+
+  bool Equals(const Path &path) {
     return std::equal(this->begin(), this->end(), path.begin());
-  }
-
-  void append(int x, int y) {
-    this->push_back(Cell(x, y));
-  }
-
-  void append(const Cell &cell) {
-    this->push_back(cell);
   }
 
  private:
@@ -56,43 +30,24 @@ class Path : public std::list<Cell> {
 
 class Labyrinth {
  public:
-  /* A labyrinth is described by a two-dimensional array.
-     0 means you can go there.
-     1 means you can't go there.
-   */
-  Labyrinth(int *field, int width, int height) {
-    field_ = new int[width * height];
-    memcpy(field_, field, sizeof(field_[0]) * width * height);
-    width_ = width;
-    height_ = height;
-  }
+  // A labyrinth is described by a two-dimensional array.
+  // 0 means you can go there.
+  // 1 means you can't go there.
+  // The first index numbers array rows from top to down starting from 0.
+  // The second index numbers array columns from left to right starting from 0.
+  // Please note the order of height and width arguments.
+  Labyrinth(int *field, int height, int width);
 
-  ~Labyrinth() {
-    delete [] field_;
-  }
+  ~Labyrinth();
 
-  bool FindPath(const Cell &start_cell,
-                const Cell &end_cell,
+  bool FindPath(const LabyrinthPoint &start_point,
+                const LabyrinthPoint &end_point,
                 Path *found_path) const;
-
-  int width() const { return width_; }
-  int height() const { return height_; }
 
  private:
   int *field_;
   int height_;
   int width_;
-
-  void GetAdjacentCells(const Cell &cell,
-                        std::vector<Cell> *adjacent_cells) const;
-
-  bool IsEmpty(const Cell &cell) const {
-    DCHECK_GE(cell.x, 0);
-    DCHECK_LT(cell.x, width_);
-    DCHECK_GE(cell.y, 0);
-    DCHECK_LT(cell.y, height_);
-    return *(field_ + cell.x + cell.y * width_) == 0;
-  }
 
   DISALLOW_COPY_AND_ASSIGN(Labyrinth);
 };
