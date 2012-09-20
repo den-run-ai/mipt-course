73db3f8c2b95132d9a3585e325f7c3a22a3c39c3
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2012-09-20 18:24:22 +0000

    Translate the codestyle sample #1 to Russian

--- trunk/demos/codestyle/1_variables_and_functions.cpp
+++ trunk/demos/codestyle/1_variables_and_functions.cpp
@@ -3,101 +3,129 @@
 
 #include <string>
 
-// http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Static_and_Global_Variables#Static_and_Global_Variables
-int AVOID_GLOBALS;
-
-// http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Function_Declarations_and_Definitions#Function_Declarations_and_Definitions
-// Function names - camel case
-int DeclareSimpleFunctionsLikeThis(int a, int b) {
-  return a + b;  // note the 2 spaces indentation.
-}
-
-struct Result {
-  char value;
-  int some_other_data;
-};
-/* http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Function_Parameter_Ordering#Function_Parameter_Ordering
- * POD in parameters - pass by value,
- * Compound in parameters - pass by const reference,
- * OUT parameters - pass by pointer, only at the end.
-
- WHY?
-  int i = 42;
-  std::string str = "ZZ";
-  struct Result s;
-  DeclareComplexFunctionsLikeThis(i, str, &s);
-  -- it's easy to see that 'i' and 'str' are passed read-only
-     and 's' will likely be changed. 
- */
-void DeclareComplexFunctionsLikeThis(int in_parameter,
-                                     const std::string &in_compound_parameter,
-                                     struct Result *out_parameter) {
-  out_parameter->value = in_compound_parameter.c_str()[in_parameter];
-}
-
-
-int main() {
-  // variable names - lowercase with underscore:
+void VariablesAndStatemants() {
+  // Переменные называются ёмкими понятными именами, маленькими буквами
+  // с отделением слов подчёркиванием.
   int example_variable = 0;
 
-  // pointers - asterisks at the name.
-  // Remember: in C++, "*" applies to the variable name, not the type!!!
+  // Указатели - звёздочки обязательно НЕ отделять пробелом от имени.
+  // Напоминаю: в C++, "*" относится к имени переменной, а не к типу!!!
   int *pointer_definition = &example_variable, *pointer2;
   // NOT "int* pointer1, pointer2;"
 
-  // Some examples of if/else, for, while
+  // Примеры if/else, for, while...
   if (example_variable == 1) {
     printf("Shouldn't be printed\n");
   }
-  // NOT "if(condition){"
-  // NOT "if (condition){"
-  // NOT "if(condition) {"
+  // НЕ "if(condition){",
+  // НЕ "if (condition){",
+  // НЕ "if(condition) {" !
 
   if (*pointer_definition == 1) {
-    printf("Accessing pointers should not add extra spaces!\n");
+    printf("НЕ ставьте лишних пробелов при разыменовывании указателей !\n");
   }
 
   if (example_variable == 1) {
-    printf("Shouldn't be printed\n");
+    printf("Не должно быть распечатано\n");
   } else {
-    printf("This program is written according "
-           "to Google C++ Code Style Guide\n");
-    printf("See http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml"
-           " for the details.\n\n");
+    printf("Программа написана в соответствии "
+           "с Google C++ Code Style Guide\n");
+    printf("Подробнее см тут:\n"
+           "http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml"
+           "\n\n");
   }
 
-  printf("As you've noticed, 1 indent = 2 spaces, not tabs.\n");
-  printf("Also, we have a 80-char line limit.\n\n");
+  printf("Как вы заметили, 1 отступ = 2 пробела, НЕ таба.\n");
+  printf("Ограничение на длину строки - 80 символов.\n\n");
 
   if (example_variable == 1)
-    printf("You can avoid {} for one-line statements\n");
+    printf("Можно не писать {} для однострочных выражений\n");
   else
-    printf("Indeed,\nif (condition)\n  statement;\nis compact but ");
+    printf("Конечно,\nif (condition)\n  statement;\nудобно, но ");
 
   if (example_variable == 1)
-    printf("You can avoid {} for one-line statements but ");
-    printf("be careful when using if without {}!\n");  // OOPS! This is not
-                                                       // detected by cpplint!
-
+    printf("Можно не писать {} для однострочных выражений, но ");
+    printf("тогда будьте внимательны к отступам {}!\n");  // OOPS! Не находится
+                                                          // с помощью cpplint!
   for (int i = 0; i < 1; ++i)
-    printf("\nThe same {} rules apply to for");
+    printf("\nТе же правила про {} применяются к циклу for");
 
   while (example_variable++ < 1) {
-    printf(" and while loops.\n");
+    printf(" и while.\n");
   }
 
-  // You can have small if (condition) statement on one line if that's readable:
+  for (int i = 0; i < 1; ++i) {
+    printf("Определяйте переменные");
+    // Вы наверное уже обратили внимание, что у каждого
+    // цикла for - своя переменная i?
+    std::string example_string = " с минимально возможной областью видимости!"
+                                 "\nЭто упростит поиск определения переменной"
+                                 " при работе с большими функциями.";
+  }
+
+  // Можно писать if (condition) statement на одной строке,
+  // если это не ухудшает читаемость:
   if (example_variable == 1) example_variable = 0;
+}
 
+// http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Static_and_Global_Variables#Static_and_Global_Variables
+int avoid_globals_whenever_possible;
 
-  // Some examples of calling functions and declaring local variables {{{1
-  DeclareSimpleFunctionsLikeThis(1, 2);
+// Константы лучше определять так:
+const int CONSTANT_NAME = 42;
+// или так:
+const int kConstantName = 42;
+// но не через define.
 
-  std::string example_string = "Define local variables with as narrow scope as "
-                               "possible to help finding the declaration in "
-                               "large functions.";
+// http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Function_Declarations_and_Definitions#Function_Declarations_and_Definitions
+// Имена функций - CamelCase.
+int SimpleFunction(int a, int b) {
+  return a + b;  // Заметьте: отступ!
+}
+
+void CallSimpleFunction() {
+  // Вызов простой функции.
+  SimpleFunction(1, 2);
+}
+
+// Объявления структур:
+struct Result {
+  char some_field;
+  int some_other_data;
+};
+
+struct OtherStruct {
+  int some_field;
+};
+
+/* http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml?showone=Function_Parameter_Ordering#Function_Parameter_Ordering
+ * "POD" параметры (int, float, ...) - передавать по значению,
+ * Составные входные параметры - по константной ссылке,
+ * OUT/выходные параметры - по указателю; в последнюю очередь.
+ * Позже будет пример почему ...
+ */
+void ComplexFunction(int in_parameter,
+                     const std::string &in_compound_parameter,
+                     struct Result *out_parameter) {
+  struct OtherStruct other;
+  other.some_field = 42;
+
+  out_parameter->some_field = in_compound_parameter.c_str()[in_parameter];
+}
+
+void CallComplexFunction() {
+  std::string example_string = "Привет!";
   struct Result result;
-  DeclareComplexFunctionsLikeThis(3, example_string, &result);
+  ComplexFunction(3, example_string, &result);
+  // Сразу видно что example_string не будет изменена, а result - будет!
+}
+
+int main() {
+  VariablesAndStatemants();
+
+  CallSimpleFunction();
+
+  CallComplexFunction();
 
   return 0;
 }
