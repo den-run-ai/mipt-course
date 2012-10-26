a31364769e3891f7b0fc3dcceaadb8a5fe63505d
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2012-10-26 19:47:22 +0000

    Improve the googletest demo

--- demos/googletest/2_fixture.cpp
+++ demos/googletest/2_fixture.cpp
@@ -1,27 +1,36 @@
-#include <gtest/gtest.h>
 #include <stdio.h>
+
+#include <gtest/gtest.h>
 
 // http://code.google.com/p/googletest/wiki/Primer#Test_Fixtures:_Using_the_Same_Data_Configuration_for_Multiple_Te
 
+// Некое подобие класса-интерфейса для базы данных.
+// Практический смысл отсутствует, просто демонстрация.
 class Database {
  public:
   Database()  { printf("DB created\n"); }
   ~Database() { printf("DB destroyed\n"); }
 
-  bool Connect() { printf("Connected!\n"); return true; }
+  bool Connect() {
+    printf("Connected!\n");
+    return true;
+  }
+
   void ReleaseResources() { printf("Released...\n"); }
 
-  void DoSomething() { }
-  void DoSomethingElse() { }
+  void DoSomething() { printf("Doing something\n"); }
+  void DoSomethingElse() { printf("Doing something else\n"); }
 };
 
 // A new instance is created for each test case being run.
 class DatabaseTest : public ::testing::Test {
  protected:
+  // Вызывается перед запуском каждого теста.
   virtual void SetUp() {
     ASSERT_TRUE(database_.Connect());
   }
 
+  // Вызывается после каждого теста, даже в случае FAILURE.
   virtual void TearDown() {
     database_.ReleaseResources();
   }
