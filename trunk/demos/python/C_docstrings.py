3455a9ccb5d4a293789e26040b095d257c16b7ed
timurrrr <timurrrr@25c5acd0-bda5-8b57-2d56-82901bac209b>
2012-11-30 14:32:50 +0000

    Moar

---
+++ trunk/demos/python/C_docstrings.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Docstring demo
+
+Запускать так:
+$ python
+>>> import E_docstrings
+>>> help(E_docstrings)
+>>> help(E_docstrings.foo)
+"""
+
+def foo():
+    """ This function has a docstring!
+    
+    You can write some useful tips here.
+    See http://www.python.org/dev/peps/pep-0257/
+    for the general docstring guidelines.
+    """
+    print "Hello"
+
+if __name__ == "__main__":
+    foo()
