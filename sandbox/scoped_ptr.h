d4ac74f49d64477a72ad634b7f2255b4f66db511
tatunov505 <tatunov505@25c5acd0-bda5-8b57-2d56-82901bac209b>
2012-12-19 07:20:36 +0000

    Added interface and DISABLED tests for scoped_ptr<T>. Reviewed at http://codereview.appspot.com/6947043
    Update issue 30

---
+++ sandbox/scoped_ptr.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2012 Timur Iskhodzhanov and MIPT students. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SANDBOX_SCOPED_PTR_H_
+#define SANDBOX_SCOPED_PTR_H_
+
+#include "base/common.h"
+
+// A scoped_ptr<T> is just like T* except its destructor deletes
+// the pointer it holds (if any).
+// That is, scoped_ptr<T> owns T object it points to.
+// Like T*, scoped_ptr<T> may hold NULL or pointer to a T object.
+//
+// Note: reset(...) also deletes the stored ponter. Use release() if you want
+// scoped_ptr<T> to forget about pointer without deleting the object.
+template <class T>
+class scoped_ptr {
+ public:
+  explicit scoped_ptr(T *p = NULL) {}
+  ~scoped_ptr() {}
+
+  void reset(T *p = NULL) {}
+  T& operator*() {NOT_IMPLEMENTED;}
+  T* operator->() {NOT_IMPLEMENTED;}
+  T* get() const {NOT_IMPLEMENTED;}
+  T* release() {NOT_IMPLEMENTED;}
+  void swap(scoped_ptr& ptr) {}  // NOLINT
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(scoped_ptr);
+};
+
+#endif  // SANDBOX_SCOPED_PTR_H_
---
+++ sandbox/scoped_ptr_test.cpp
@@ -0,0 +1,94 @@
+// Copyright (c) 2012 Timur Iskhodzhanov and MIPT students. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sandbox/scoped_ptr.h"
+
+#include "gtest/gtest.h"
+
+class TestClass {
+ public:
+  explicit TestClass(int *dtor_counter) : dtor_counter_(dtor_counter) {}
+
+  ~TestClass() {
+    ++(*dtor_counter_);
+  }
+
+  int SomeMethod(int v) {return 42 + v;}
+
+ private:
+  int *dtor_counter_;
+};
+
+TEST(ScopedPtrTest, DISABLED_BasicTest) {
+  int dtor_counter = 0;
+
+  {
+    TestClass* test_obj = new TestClass(&dtor_counter);
+    scoped_ptr<TestClass> ptr(test_obj);
+    EXPECT_EQ(0, dtor_counter);
+
+    EXPECT_EQ(test_obj, ptr.get());
+    EXPECT_EQ(42042, ptr->SomeMethod(42000));
+    EXPECT_EQ(4242, (*ptr).SomeMethod(4200));
+
+    EXPECT_EQ(0, dtor_counter);
+  }
+  EXPECT_EQ(1, dtor_counter);
+}
+
+TEST(ScopedPtrTest, DISABLED_ReleaseTest) {
+  int dtor_counter = 0;
+
+  {
+    TestClass test_obj(&dtor_counter);
+
+    // Warning! A pointer to local variable is passed to scoped_ptr.
+    scoped_ptr<TestClass> ptr(&test_obj);
+
+    EXPECT_EQ(&test_obj, ptr.release());
+    EXPECT_EQ(0, dtor_counter);
+    EXPECT_EQ(NULL, ptr.release());
+  }
+  EXPECT_EQ(1, dtor_counter);
+}
+
+TEST(ScopedPtrTest, DISABLED_ResetTest) {
+  int dtor_counter_a = 0, dtor_counter_b = 0;
+
+  {
+    TestClass *a = new TestClass(&dtor_counter_a);
+    TestClass *b = new TestClass(&dtor_counter_b);
+
+    scoped_ptr<TestClass> ptr(a);
+    ptr.reset(b);
+
+    EXPECT_EQ(1, dtor_counter_a);
+    EXPECT_EQ(0, dtor_counter_b);
+  }
+  EXPECT_EQ(1, dtor_counter_a);
+  EXPECT_EQ(1, dtor_counter_b);
+}
+
+TEST(ScopedPtrTest, DISABLED_SwapTest) {
+  int dtor_counter_a = 0, dtor_counter_b = 0;
+
+  {
+    TestClass *a = new TestClass(&dtor_counter_a);
+    scoped_ptr<TestClass> ptr_a(a);
+
+    {
+      TestClass *b = new TestClass(&dtor_counter_b);
+      scoped_ptr<TestClass> ptr_b(b);
+      ptr_b.swap(ptr_a);
+
+      EXPECT_EQ(0, dtor_counter_a);
+      EXPECT_EQ(0, dtor_counter_b);
+    }
+    EXPECT_EQ(1, dtor_counter_a);
+    EXPECT_EQ(0, dtor_counter_b);
+  }
+  EXPECT_EQ(1, dtor_counter_a);
+  EXPECT_EQ(1, dtor_counter_b);
+}
+
